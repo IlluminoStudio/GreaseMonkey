f70dc9c9ccc6c7a037e4ee6efac59b874aa9d182
John Tee <illumino.studio.324@gmail.com>
2020-05-02 15:47:34 +1000

    msg

---
+++ GreaseMonkey/Views/JobCell.swift
@@ -0,0 +1,24 @@
+//
+//  JobTableViewCell.swift
+//  GreaseMonkey
+//
+//  Created by user on 29/4/20.
+//  Copyright © 2020 Test Co. All rights reserved.
+//
+
+import UIKit
+
+class JobCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
